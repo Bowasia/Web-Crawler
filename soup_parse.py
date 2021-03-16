c25096ff1fab377f59787f21981d07f04dfd01e1
bowasia <leejingsawat@gmail.com>
2021-03-17 00:01:01 +0700

    commit?

--- soup_parse.py
+++ soup_parse.py
@@ -4,6 +4,8 @@ with open("FB_rubhewtookyang_11_03_2021_16-49-51.html", "r") as fp:
 
 
 for link in soup.find_all("a", attrs={"class": ['oajrlxb2','g5ia77u1','qu0x051f','esr5mh6w','e9989ue4','r7d6kgcz','rq0escxv','nhd2j8a9','nc684nl6','p7hjln8o','kvgmc6g5','cxmmr5t8','oygrvhab','hcukyx3x','jb3vyjys','rz4wbd8a','qt6c0cv9','a8nywdso','i1ao9s8h','esuyzwwr','f1sip0of','lzcic4wl','oo9gr5id','gpro0wi8','lrazzd5p'],"aria-hidden":"true","role":"link","tabindex":"-1"}):
-    print(link.get('href'))
+    print(link.get('href')) #Profile link
+    print(link.a.get('aria-label')) #Profile name
+
     #print(link)
     #print(link.attrs['href'])
