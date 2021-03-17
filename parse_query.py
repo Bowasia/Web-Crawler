439641e0c128cfa755b26b1397e2e7dd22c957c2
Bowasia <33247452+Bowasia@users.noreply.github.com>
2021-03-17 11:12:58 +0700

    Update parse_query.py

--- parse_query.py
+++ parse_query.py
@@ -1,26 +1,27 @@
-#!/usr/bin/python3
-
-from pyquery import PyQuery as pq
-from lxml import etree
-import urllib
-with open("FB_rubhewtookyang_11_03_2021_16-49-51.html", "r") as f:
-
-    contents = f.read()
-
-    doc = pq(contents)
-    text = doc('a').html()
-    time = doc('div.du4w35lb.k4urcfbm.l9j0dhe7.sjgh65i0')
-
-    for b in time:
-        print(time.children("a.ajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4 r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.oo9gr5id.gpro0wi8.lrazzd5p").text())
+from bs4 import BeautifulSoup
+import csv
+with open("FB_rubhewtookyang_11_03_2021_16-49-51.html", "r") as fp: #automate the file name
+    soup = BeautifulSoup(fp, "html.parser")
 
 
-    #print(doc)
-    #print(time)
-    #class of span time
-    #.b6zbclly.myohyog2.l9j0dhe7.aenfhxwr.l94mrbxd.ihxqhq3m.nc684nl6.t5a262vz.sdhka5h4
 
-    #class of link
-    #.oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4 r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.oo9gr5id.gpro0wi8.lrazzd5p
+data =[]
 
-    #print(html_href)
+for link in soup.find_all("a", attrs={"class": ['oajrlxb2','g5ia77u1','qu0x051f','esr5mh6w','e9989ue4','r7d6kgcz','rq0escxv','nhd2j8a9','nc684nl6','p7hjln8o','kvgmc6g5','cxmmr5t8','oygrvhab','hcukyx3x','jb3vyjys','rz4wbd8a','qt6c0cv9','a8nywdso','i1ao9s8h','esuyzwwr','f1sip0of','lzcic4wl','oo9gr5id','gpro0wi8','lrazzd5p'],"aria-hidden":"true","role":"link","tabindex":"-1"}):
+    href = link.get('href')
+    profile_name = link.a.get('aria-label')
+    #print(link.get('href')) #Profile link
+    #print(link.a.get('aria-label')) #Profile name
+    fields = ['Name','Link']
+    Dic = {"Name":profile_name,"Link":href}
+    data.append(Dic)
+print(data)
+
+with open('profile_link_1.csv','w') as f:
+    write = csv.DictWriter(f, fieldnames = fields)
+    write.writeheader()
+    for num in data :
+        write.writerow(num)
+
+#&sk=about
+#^^^ append at the link to go to about page
