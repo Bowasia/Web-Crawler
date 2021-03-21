2696bee7a98fa86740e0cda7e9b820d6e91f8055
bowasia <leejingsawat@gmail.com>
2021-03-21 20:52:45 +0700

    so commit first then push

--- read_csv.py
+++ read_csv.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env dash
+#!/usr/bin/env bash
 from urllib.parse import urlparse
 import csv
 import os
@@ -9,10 +9,28 @@ with open('data_personal_link_1.csv') as csvfile:
     for row in reader:
              print(row['Number'], row['Name'], row['Link'])
              obj = urlparse(row['Link'])
-             open_this_link = obj[1] + obj[2] + '/about_contact_and_basic_info'
+             path = obj[2]
+             query = obj[4]
+             id = ''
+             for character in query:
+                 if character != '&':
+                     id = id + character
+                 else:
+                     break
+
+             path_reverse = str(path[::-1])
+             print(path_reverse[0:3])
+             if path_reverse[0:3] == "php":
+                 open_this_link = obj[1] + path + "?" + id + "&sk=about_contact_and_basic_info"
+             else:
+                open_this_link = obj[1] + obj[2] + '/about_contact_and_basic_info'
              print(open_this_link)
              print(obj)
              print('\n')
-print("start")
-subprocess.call("../get_profile.sh",shell=True)
-print("End")
+             os.system("firefox " + open_this_link)
+             os.system("sleep 2")
+             os.system("xdotool key alt+f sleep 0.5 key a sleep 1")
+             os.system("xdotool type " + row['Number'] + "_personal")
+             os.system("xdotool key KP_Enter sleep 2")
+             os.system("mv ~/Downloads/"+ row['Number']+'_personal.html ~/Documents/Web-Crawler/save_page/personal_page')
+             os.system("xdotool key ctrl+w")
